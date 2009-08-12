8d2092ec09c66f351b6cb058ad85617c7ce239cd
milk1000cc <info@milk1000.cc>
2009-08-12 22:49:00 +0900

    json pure でパースしてないのに json pure でパースとか書いてあった spec example 説明文を修正

--- spec/web_service/simple/parser/json_spec.rb
+++ spec/web_service/simple/parser/json_spec.rb
@@ -7,7 +7,7 @@ require 'web_service/simple/parser/json'
 
 describe WebService::Simple::Parser::JSON do
   describe '#parse_response' do
-    it 'は、json pure で JSON 文字列をパースして、その結果を返すこと' do
+    it 'は、json library で JSON 文字列をパースして、その結果を返すこと' do
       parser = WebService::Simple::Parser::JSON.new
       response = stub(:response, :content => '{ "hoge": 3, "fuga": ["a", "b"] }')
       parser.parse_response(response).should == { 'hoge' => 3, 'fuga' => ['a', 'b'] }
